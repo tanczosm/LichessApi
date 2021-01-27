d265aa0ade3d7cd5fada7a5ac37c85204e7971a5
Michael Tanczos <tanczosm@eastonsd.org>
2021-01-26 23:27:48 -0500

    Added async streaming ndjson reader

---
+++ LichessApi/LichessApi.Web/Util/HttpContentNjsonExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LichessApi.Web.Util
+{
+    /// <summary>
+    /// Consomes Ndjson streams
+    /// https://www.tpeczek.com/2020/10/consuming-json-objects-stream-ndjson.html
+    /// </summary>
+    internal static class HttpContentNdjsonExtensions
+    {
+        private static readonly JsonSerializerOptions _serializerOptions
+            = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+        public static async IAsyncEnumerable<TValue> ReadFromNdjsonAsync<TValue>(this HttpContent content, [EnumeratorCancellation] CancellationToken token = default)
+        {
+            if (content is null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            string? mediaType = content.Headers.ContentType?.MediaType;
+
+            if (mediaType is null || !mediaType.Equals("application/x-ndjson", StringComparison.OrdinalIgnoreCase))
+            {
+                throw new NotSupportedException();
+            }
+
+            Stream contentStream = await content.ReadAsStreamAsync().ConfigureAwait(false);
+
+            using (contentStream)
+            {
+                using (StreamReader contentStreamReader = new StreamReader(contentStream))
+                {
+                    while (!contentStreamReader.EndOfStream && !token.IsCancellationRequested)
+                    {
+                        yield return JsonSerializer.Deserialize<TValue>(await contentStreamReader.ReadLineAsync()
+                            .ConfigureAwait(false), _serializerOptions);
+                    }
+                }
+            }
+        }
+    }
+}
